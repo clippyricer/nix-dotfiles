dfcc329c82a83f4076e781fbf7eae343ce7cd00c
clippyricer <bannansmooth68@proton.me>
2026-03-07 19:37:32 -0500

    add flakes

--- configuration.nix
+++ configuration.nix
@@ -59,6 +59,10 @@
   # Allow unfree packages
   nixpkgs.config.allowUnfree = true;
 
+
+  # Nix Flakes
+  nix.settings.experimental-features = [ "nix-command" "flakes" ];
+
   # List packages installed in system profile. To search, run:
   # $ nix search wget
   environment.systemPackages = with pkgs; [
