1a49197977019e7aec7e65fe759a1da5df39cdc3
clippyricer <bannansmooth68@proton.me>
2026-03-07 19:51:04 -0500

    updae flake

--- flake.nix
+++ flake.nix
@@ -1,15 +1,22 @@
 {
-  description = "A very basic flake";
+
+  description = "MY FLAKE USE IT";
 
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
   };
 
-  outputs = { self, nixpkgs }: {
-
-    packages.x86_64-linux.hello = nixpkgs.legacyPackages.x86_64-linux.hello;
-
-    packages.x86_64-linux.default = self.packages.x86_64-linux.hello;
-
+  outputs = { self, nixpkgs, ... }:
+    let
+      pkgs = nixpkgs.legacyPackages.x86_64-linux;
+      lib = nixpkgs.lib;
+    in {
+    nixosConfigurations = {
+      nixos = lib.nixosSystem {
+        system = "x86_64-linux";
+        modules = [ ./configuration.nix ];
+      };
+    };
   };
+
 }
